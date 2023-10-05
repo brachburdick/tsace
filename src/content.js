4f0cc81f9932d673d9f07aa61a43f18b9d85e745
Brach Burdick <brach.burdick@gmail.com>
2023-10-05 17:49:45 -0400

    Updated from distilbert-base-uncased-finetuned-sst-2-english to xtremedistill-go-emotion-int8

--- src/content.js
+++ src/content.js
@@ -1,70 +1,76 @@
-const MAX_CONCURRENT_ANALYSIS = 5; 
-let currentAnalyses = 0; 
-
+const MAX_CONCURRENT_ANALYSIS = 5;
+let currentAnalyses = 0;
 
 function getSentimentFromBackground(text) {
-    return new Promise((resolve, reject) => {
-        chrome.runtime.sendMessage({ action: 'analyzeTweet', tweetText: text}, (response) => {
-            if (chrome.runtime.lastError) {
-                console.error('Message sending error:', chrome.runtime.lastError);
-                reject(chrome.runtime.lastError);
-            } else {
-                resolve(response);
-            }
-        });
-    });
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage(
+      { action: "analyzeTweet", tweetText: text },
+      (response) => {
+        if (chrome.runtime.lastError) {
+          console.error("Message sending error:", chrome.runtime.lastError);
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+  });
 }
 
 async function colorizeTweet(tweet) {
-    if (currentAnalyses >= MAX_CONCURRENT_ANALYSIS) {
-        return; 
-    }
-    currentAnalyses++;
-    tweet.setAttribute('data-holdAnalysis', 'true'); 
-    const textContent = tweet.innerText;
+  if (currentAnalyses >= MAX_CONCURRENT_ANALYSIS) {
+    return;
+  }
+  currentAnalyses++;
+  tweet.setAttribute("data-holdAnalysis", "true");
+  const textContent = tweet.innerText;
 
-    try {
-        const result = await getSentimentFromBackground(textContent);
-        
-        switch (result.sentiment) {
-            case 'POSITIVE':
-                tweet.style.backgroundColor = '#566659';
-                break;
-            case 'NEGATIVE':
-                tweet.style.backgroundColor = '#614243';
-                break;
-            case 'NEUTRAL':
-            default:
-                break;
-        }
-        
-        tweet.setAttribute('data-colored', 'true'); 
-        tweet.removeAttribute('data-holdAnalysis'); 
-    } catch (error) {
-        tweet.removeAttribute('data-holdAnalysis'); 
-        console.error('Error getting sentiment:', error);
+  try {
+    const result = await getSentimentFromBackground(textContent);
+    // prev: result comes back as all-caps string of outcome
+    // update to: result comes back as
+    console.log("back in colorizeTweet", result, result.sentiment);
+    switch (result.sentiment) {
+      case "POSITIVE":
+        tweet.style.backgroundColor = "#566659";
+        break;
+      case "NEGATIVE":
+        tweet.style.backgroundColor = "#614243";
+        break;
+      case "NEUTRAL":
+      default:
+        break;
     }
-    currentAnalyses--;
+
+    tweet.setAttribute("data-colored", "true");
+    tweet.removeAttribute("data-holdAnalysis");
+  } catch (error) {
+    tweet.removeAttribute("data-holdAnalysis");
+    console.error("Error getting sentiment:", error);
+  }
+  currentAnalyses--;
 }
 
 let isColorizing = false;
 const colorizeTweets = async () => {
-    isColorizing = true;
-    const tweets = document.querySelectorAll('article[role="article"]:not([data-colored]):not([data-holdAnalysis])');
+  isColorizing = true;
+  const tweets = document.querySelectorAll(
+    'article[role="article"]:not([data-colored]):not([data-holdAnalysis])'
+  );
 
-    for (let tweet of tweets) {
-        await colorizeTweet(tweet);
-    }
-    isColorizing = false;
+  for (let tweet of tweets) {
+    await colorizeTweet(tweet);
+  }
+  isColorizing = false;
 };
 
 let scrollTimeout;
-document.addEventListener('scroll', () => {
-    if (isColorizing || currentAnalyses >= MAX_CONCURRENT_ANALYSIS) return;
+document.addEventListener("scroll", () => {
+  if (isColorizing || currentAnalyses >= MAX_CONCURRENT_ANALYSIS) return;
 
-    chrome.storage.sync.get('isColorizingEnabled', function(data) {
-        if (data.isColorizingEnabled) {
-            colorizeTweets();
-        }
-    });
+  chrome.storage.sync.get("isColorizingEnabled", function (data) {
+    if (data.isColorizingEnabled) {
+      colorizeTweets();
+    }
+  });
 });
